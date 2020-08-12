c47b287c795e0b631920cfdaa88a1004cbdc6e04
berdiyeva <berdiyevaaltyn@gmail.com>
2020-08-11 19:32:08 -0500

    mvp code is here

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,31 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+# with open('src/foo.txt', 'r') as f:
+#     for line in f:
+#         print(line, end='')
+#     f.close()
+
+with open('src/foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
+print(f.closed)
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+y = open("bar.txt", "w")
+y.write("this is first line.\n")
+y.write("this is second line.\n")
+y.write("this is third line.")
+y.close()
+print(y.closed)
+with open("bar.txt") as y:
+  print("\n" + y.read())
+print(y.closed)
